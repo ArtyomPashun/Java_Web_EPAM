16b33f829433944d98ca8ad4462228e66765bd88
ADMIN <artyom.pashun@gmail.com>
2021-09-27 23:44:29 +0300

    Task_1

--- src/by/training/task1/lastNumber/LastNumber.java
+++ src/by/training/task1/lastNumber/LastNumber.java
@@ -9,22 +9,22 @@ package by.training.task1.lastNumber;
 
 public class LastNumber {
 
-    public void lastNumber(int x) {
+    public void lastNumber(int number) {
 
         String lastNumber = "Последняя цифра квадрата числа ";
 
-        if (x % 10 == 0) {
-            System.out.println(lastNumber + x + "-> 0");
-        } else if ((x % 10 == 1) || (x % 10 == 9)) {
-            System.out.println(lastNumber + x + "-> 1");
-        } else if ((x % 10 == 2) || (x % 10 == 8)) {
-            System.out.println(lastNumber + x + "-> 4");
-        } else if ((x % 10 == 3) || (x % 10 == 7)) {
-            System.out.println(lastNumber + x + "-> 9");
-        } else if ((x % 10 == 4) || (x % 10 == 6)) {
-            System.out.println(lastNumber + x + "-> 6");
-        } else if (x % 10 == 5) {
-            System.out.println(lastNumber + x + "-> 5");
+        if (number % 10 == 0) {
+            System.out.println(lastNumber + number + "-> 0");
+        } else if ((number % 10 == 1) || (number % 10 == 9)) {
+            System.out.println(lastNumber + number + "-> 1");
+        } else if ((number % 10 == 2) || (number % 10 == 8)) {
+            System.out.println(lastNumber + number + "-> 4");
+        } else if ((number % 10 == 3) || (number % 10 == 7)) {
+            System.out.println(lastNumber + number + "-> 9");
+        } else if ((number % 10 == 4) || (number % 10 == 6)) {
+            System.out.println(lastNumber + number + "-> 6");
+        } else if (number % 10 == 5) {
+            System.out.println(lastNumber + number + "-> 5");
         }
     }
 }
