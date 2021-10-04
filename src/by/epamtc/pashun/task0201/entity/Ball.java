fbff8d390628cc8961bc7ad0a17b9256865bbfe7
ADMIN <artyom.pashun@gmail.com>
2021-10-04 23:20:37 +0300

    Task_2

--- src/by/epamtc/pashun/task0201/entity/Ball.java
+++ src/by/epamtc/pashun/task0201/entity/Ball.java
@@ -6,20 +6,42 @@ public class Ball {
     private double weight = 0;
 
     public Ball(double weight, BallColour colour) {
-
         this.weight = weight;
         this.colour = colour;
     }
 
     public double getWeight() {
-
         return weight;
     }
 
     public BallColour getColour() {
-
         return colour;
     }
-}
 
-// не переопределил методы toString, hashCode, equals
+    @Override
+    public int hashCode() {
+        final int prime = 37;
+        int result = 1;
+        result = prime * result + ((colour == null) ? 0 : colour.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if ((obj == null) || (getClass() != obj.getClass())) {
+            return false;
+        }
+        Ball ball = (Ball) obj;
+        return weight == ball.weight && ((colour == ball.colour) || (colour != null && colour.equals(ball.getColour())));
+    }
+
+    @Override
+    public String toString() {
+        return getClass().getName() + '@'
+                + "colour: " + colour
+                + ", weight: " + weight;
+    }
+}
--- src/by/epamtc/pashun/task0201/entity/Basket.java
+++ src/by/epamtc/pashun/task0201/entity/Basket.java
@@ -1,6 +1,7 @@
 package by.epamtc.pashun.task0201.entity;
 
 import java.util.ArrayList;
+import java.util.Objects;
 
 public class Basket {
 
@@ -8,7 +9,7 @@ public class Basket {
 
     private double totalWeight = 0;
 
-    private int blueBalls = 0;
+    private int colourBalls = 0;
 
     public Basket() {
 
@@ -31,11 +32,38 @@ public class Basket {
 
         for (Ball ball : basketArray) {
             if (ball.getColour() == colour) {
-                blueBalls++;
+                colourBalls++;
             }
         }
-        return blueBalls;
+        return colourBalls;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 37;
+        int result = 1;
+        result = prime * result + ((basketArray == null) ? 0 : basketArray.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if ((obj == null) || (getClass() != obj.getClass())) {
+            return false;
+        }
+        Basket basket = (Basket) obj;
+        return (totalWeight == basket.totalWeight) && (colourBalls == basket.colourBalls)
+                && (Objects.equals(basketArray, basket.basketArray));
+
+    }
+
+    @Override
+    public String toString() {
+        return getClass().getName() + '@'
+                + "total weight: " + totalWeight
+                + ", colour balls: " + colourBalls;
     }
 }
-
-// не переопределил методы toString, hashCode, equals
